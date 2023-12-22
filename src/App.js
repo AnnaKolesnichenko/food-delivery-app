2fd102648cc16c553a296dc6578b0e964ffa2ebe
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-12-22 14:07:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,12 +40,12 @@ function App() {
         </NavigationContainer> */}
         <HeaderComponent />
         <Routes>
-          <Route path="/" element={<MainPage />}>
+          <Route path="" element={<MainPage />}>
             {" "}
           </Route>{" "}
-          <Route path="/sushi" element={<SushiPage />} />
-          <Route path="/pizza" element={<FastFoodPage />} />
-          <Route path="/salads" element={<SaladsPage />} />
+          <Route path="sushi" element={<SushiPage />} />
+          <Route path="pizza" element={<FastFoodPage />} />
+          <Route path="salads" element={<SaladsPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </ContainerMain>
